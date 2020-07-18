8bc2215ed8cba8cfe3babb081098f306dbf20c1c
nlee0212 <nlee0212@gmail.com>
2020-07-18 20:43:24 +0900

    Get image list from dir

--- 200715 Image Washing Trial.py
+++ 200715 Image Washing Trial.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
 import matplotlib as plt
+import os
 
 def image_wash(filename):
     img = plt.imread(filename)
 
 
-image_wash('')
+path = "./"
+file_list = os.listdir(path)
+file_list_image = [file for file in file_list if file.endswith(".jpg",".png")]
+print(file_list_image)
