8eeda5b85f36b7f963af271a9d2662a9f7ba6d69
nlee0212 <nlee0212@gmail.com>
2020-07-19 02:47:29 +0900

    Edit Image Washing Process

--- 200715 Image Washing Trial.py
+++ 200715 Image Washing Trial.py
@@ -8,52 +8,42 @@ from google.cloud import vision
 from google.cloud.vision import types
 
 def image_wash(filename):
-    # get image name
     img_full = filename.split('.')
     img_name = img_full[0]
-    img = cv2.imread(filename,cv2.IMREAD_GRAYSCALE)
 
-    """#RGB -> Gray
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('gray' + img_name+'.'+img_full[1], gray_img)
-    cv2.imshow('gray', gray_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()"""
+    large = cv2.imread(filename)
+    rgb = cv2.pyrDown(large)
+    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 
-    """#MorphGradient
-    morph_grad_img = cv2.morphologyEx(img,cv2.MORPH_GRADIENT,(2,2))
-    cv2.imwrite('mg' + img_name+'.'+img_full[1], morph_grad_img)
-    cv2.imshow('mg', morph_grad_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()"""
-
-    # Adaptive Threshold
-    adap_thresh_img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 10)
-    cv2.imwrite('at' + img_name + '.' + img_full[1], adap_thresh_img)
-    cv2.imshow('at', adap_thresh_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    #MorphClose
-    morph_close_img = cv2.morphologyEx(adap_thresh_img,cv2.MORPH_CLOSE,(20,5))
-    cv2.imwrite('mc' + img_name + '.' + img_full[1], morph_close_img)
-    cv2.imshow('mc', morph_close_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    contours, _ = cv2.findContours(morph_close_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        rect = cv2.rectangle(adap_thresh_img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-        if h<10 or w<40:
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+    cv2.imshow('mg', grad)
+    cv2.imwrite('mg' + img_name + '.' + img_full[1], grad)
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    cv2.imshow('t', bw)
+    cv2.imwrite('t' + img_name + '.' + img_full[1], bw)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow('mc', connected)
+    cv2.imwrite('mc' + img_name + '.' + img_full[1], connected)
+    # using RETR_EXTERNAL instead of RETR_CCOMP
+    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    mask = np.zeros(bw.shape, dtype=np.uint8)
+    for idx in range(len(contours)):
+        height, width = bw.shape
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y + h, x:x + w] = 0
+        # 가장자리의 글씨들은 주된 문제가 아닐 것이기 때문에 제외.
+        if x + w == width or x == 0 or y + h == height or y == 0:
             continue
-        cv2.imshow('cnt',rect)
-        cv2.waitKey(0)
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
+        if r > 0.3 and w > 10 and h > 10:
+            cv2.rectangle(rgb, (x, y), (x + w - 1, y + h - 1), (0, 255, 0), 2)
 
-        # Cropping the text block for giving input to OCR
-        cropped = adap_thresh_img[y:y + h, x:x + w]
-        cv2.imwrite('crop' + img_name + '.' + img_full[1], cropped)
-        text_extract('crop' + img_name + '.' + img_full[1])
+    # show image with contours rect
+    cv2.imshow('rects', rgb)
+    cv2.waitKey()
 
 def text_extract(path):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Program Files/VisionAPI/rosy-clover-282218-a95092db74bf.json"
