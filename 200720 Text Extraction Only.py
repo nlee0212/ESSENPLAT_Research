26f59d4442654a0711d3886341619dc792541877
nlee0212 <nlee0212@gmail.com>
2020-07-20 01:33:51 +0900

    Filename Modification

--- 200720 Text Extraction Only.py
+++ 200720 Text Extraction Only.py
@@ -108,7 +108,7 @@ def text_extract(file_list, filename_list):
         except IndexError:
             continue
 
-    with io.open(filename_list[0]+filename_list[1],'rb') as image_file:
+    with io.open(filename_list[0]+'.'+filename_list[1],'rb') as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
     response = client.text_detection(image=image)
