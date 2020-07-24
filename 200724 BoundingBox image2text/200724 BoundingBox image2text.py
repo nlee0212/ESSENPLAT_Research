735f3f2ea21da1c85d35ff7bd79dbbf8ae13c962
nlee0212 <nlee0212@gmail.com>
2020-07-24 21:50:07 +0900

    Image Crop & OCR done

--- 200724 BoundingBox image2text/200724 BoundingBox image2text.py
+++ 200724 BoundingBox image2text/200724 BoundingBox image2text.py
@@ -7,6 +7,7 @@ import re
 from google.cloud import vision
 from google.cloud.vision import types
 import json
+from math import floor, ceil
 
 def image_crop(ori_img,img,img_full,data,h):
     """
@@ -19,16 +20,32 @@ def image_crop(ori_img,img,img_full,data,h):
     """
     img_name = img_full[0]
     crop_filename_list = list()
-    rect_img = ori_img
+    rect_img = ori_img.copy()
     for i in range(len(data)):
         box = data[i]["boundingBox"]
-        rect_img = cv2.rectangle(rect_img,(box[0][0],h-box[0][1]),(box[2][0],h-box[2][1]),
-                                 (0,255,0),2)
-        cropped = img[box[2][1]:box[0][1], box[0][0]:box[1][0]]
+        wl = min(floor(box[0][0]),floor(box[2][0]))
+        if wl<0:
+            wl=0
+        wr = max(ceil(box[0][0]),ceil(box[2][0]))
+        if wr<0:
+            wr=0
+        hl = min(floor(box[0][1]), floor(box[2][1]))
+        if hl<0:
+            hl=0
+        hr = max(ceil(box[0][1]), ceil(box[2][1]))
+        if hr<0:
+            hr=0
+        rect_img = cv2.rectangle(rect_img,(wl,hl),(wr,hr),color = (0,255,0),thickness=2)
+        cropped = img[hl:hr, wl:wr]
+        print(box)
+        #cv2.imshow('crop',cropped)
         crop_filename = 'crop' + str(i) + img_name + '.' + img_full[1]
         cv2.imwrite(crop_filename, cropped)
         crop_filename_list.append(crop_filename)
-    cv2.imshow(img_full[0],rect_img)
+    #cv2.imshow(img_full[0],rect_img)
+    cv2.imwrite('rect'+img_name+'.'+img_full[1],rect_img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     text_extract(crop_filename_list, img_full)
 
@@ -37,7 +54,7 @@ def image_wash(filename):
     img_full = filename.split('.')
     img_name = img_full[0]
 
-    img = cv2.imread(filename,0)
+    img = cv2.imread("./boundingbox_image/"+filename,0)
 
     #img_thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,2)
     #cv2.imshow('thresh', img_thresh)
@@ -61,11 +78,11 @@ def image_wash(filename):
 
     #cv2.imshow('sharp', img_sharp)
     cv2.imwrite('final'+filename,img_cl)
-    test_text('final'+filename)
+    #test_text('final'+filename)
     """cv2.waitKey()
     cv2.destroyAllWindows()"""
 
-    return img, img_sharp, img_full
+    return img, img_cl, img_full
 
 def test_text(filename):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Program Files/VisionAPI/rosy-clover-282218-a95092db74bf.json"
@@ -81,7 +98,7 @@ def test_text(filename):
     print('Modified Texts:')
     try:
         print(texts[0].description)
-        modified_text += ' ' + texts[0].description
+        modified_text += ' ' + texts[0].description.strip()
     except IndexError:
         pass
     filename += ".txt"
@@ -104,11 +121,11 @@ def text_extract(file_list, filename_list):
         print('Modified Texts:')
         try:
             print(texts[0].description)
-            modified_text += ' '+texts[0].description
+            modified_text += ' '+texts[0].description.strip()
         except IndexError:
             continue
 
-    with io.open(filename_list[0]+'.'+filename_list[1],'rb') as image_file:
+    with io.open("./boundingbox_image/"+filename_list[0]+'.'+filename_list[1],'rb') as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
     response = client.text_detection(image=image)
@@ -124,21 +141,21 @@ def text_extract(file_list, filename_list):
     with open(filename, "w", encoding='utf-8') as fp:
         fp.write("원본:\n")
         fp.write(ori_text+"\n")
-        fp.write("\n전처리후:\n")
+        fp.write("\n전처리 & crop 후:\n")
         fp.write(modified_text)
 
-path = "./"
+path = "C:/Users/user/PycharmProjects/ESSENPLAT_Research/200724 BoundingBox image2text/boundingbox_image"
 file_list = os.listdir(path)
 file_list_image = [file for file in file_list if file.endswith(".jpg")]
-file_list_png = [file for file in file_list if file.endswith(".PNG")]
+file_list_png = [file for file in file_list if file.endswith(".PNG") or file.endswith(".png")]
 file_list_image += file_list_png
 
 for image in file_list_image:
     print(image)
-    """filename = image.split(".")[0]+".json"
-    with open(filename) as json_file:
+    filename = image.split(".")[0]+".txt"
+    with open("./boundingbox_image/"+filename, 'rt', encoding="UTF8") as json_file:
         json_data = json.load(json_file)
-    """
+
     img, img_sharp, img_full = image_wash(image)
-    """height = json_data["meta"]["img_size"][0]["height"]
-    image_crop(img,img_sharp,img_full,json_data["words"],height)"""
+    height = json_data["meta"]["img_size"][0]["height"]
+    image_crop(img,img_sharp,img_full,json_data["words"],height)
